fd3099f7390711af63104eab1d07a0bab977e9d3
Ikond2 <sickofmushes@gmail.com>
2024-04-03 19:37:52 +0300

    Add project files.

---
+++ Car4U/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Car4U.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
