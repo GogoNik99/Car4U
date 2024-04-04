41e72a1365467d5a6344e21ec63518f60333ef41
Ikond2 <sickofmushes@gmail.com>
2024-04-04 19:38:37 +0300

    Revert "Revert "Identity User added to Owner Db entity""
    This reverts commit 5a56a02b46061b7ce29a9d1317cb3faca5d49880.

---
+++ Car4U.Infrastructure/Migrations/20240404104435_IdentityUserAddedToOwner.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Car4U.Infrastructure.Migrations
+{
+    public partial class IdentityUserAddedToOwner : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Owners",
+                type: "nvarchar(450)",
+                nullable: false,
+                comment: "User Identifier",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldComment: "User Identifier");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Owners_UserId",
+                table: "Owners",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Owners_AspNetUsers_UserId",
+                table: "Owners",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Owners_AspNetUsers_UserId",
+                table: "Owners");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Owners_UserId",
+                table: "Owners");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Owners",
+                type: "nvarchar(max)",
+                nullable: false,
+                comment: "User Identifier",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldComment: "User Identifier");
+        }
+    }
+}
