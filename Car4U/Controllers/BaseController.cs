3801caa187f575a081c7cca2c4e6446b1cfb0a97
GogoNik99 <georginikolov383@gmail.com>
2024-04-05 18:04:59 +0300

    creating controllers

---
+++ Car4U/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Car4U.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+
+    }
+}
