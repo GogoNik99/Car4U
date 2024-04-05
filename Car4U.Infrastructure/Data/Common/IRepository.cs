57e9635acff2c2f4150ba3b25db3018e72b7d0b8
GogoNik99 <georginikolov383@gmail.com>
2024-04-05 15:57:39 +0300

    Repository Added

---
+++ Car4U.Infrastructure/Data/Common/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Car4U.Infrastructure.Data.Common
+{
+    public interface IRepository
+    {
+        IQueryable<T> All<T>() where T : class;
+
+        IQueryable<T> AllReadOnly<T>() where T : class;
+
+        Task AddAsync<T>(T entity) where T : class;
+
+        Task DeleteAsync<T>(object id) where T : class;
+
+        Task<T> GetByIdAsync<T>(object id) where T : class;
+    }
+}
